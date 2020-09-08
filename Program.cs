9068df0c5b4f2b209bc06e2cbc8f46cdf3ee576f
DANIEL GROULX <trakmate@gmail.com>
2020-09-07 22:24:40 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace DansGame
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Console.WriteLine("Enter a number from 0 to 100");
+            userInput = Console.ReadLine();
+            if (userInput == "5")
+            {
+                Console.WriteLine("You win!!!");
+
+            }
+            else Console.WriteLine("You lose!!");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
